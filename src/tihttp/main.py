e91adcdf72a4a4fc74b294751e4958b2ba83b048
Niklas Tiede <niklastiede2@gmail.com>
2021-02-20 18:33:13 +0100

    adding a test

--- src/tihttp/main.py
+++ src/tihttp/main.py
@@ -4,9 +4,14 @@ import collections
 import sys
 
 
-def main(argv):
+def main(argv=None):
+
+    # give test suite access to CLI
+    if not argv:
+        argv = sys.argv[1:]
+
     parser = build_parser()
-    args = parser.parse_args(argv[1:])
+    args = parser.parse_args(argv)
 
     url = ""
 
@@ -79,7 +84,7 @@ def build_parser():
 
 def run_main():
     try:
-        sys.exit(main(sys.argv))
+        sys.exit(main())  # sys.argv
     except Exception as e:
         sys.stderr.write(e)
         sys.exit(1)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,54 +1,32 @@
-import pytest
-import os
-
-
-# parser = build_parser(['tihttp', 'google.com'])
-# print(parser)
-
-# x = print(os.system("python --version"))
-# print(x)
-
-# def test_parser():
-#     parser = build_parser(['-l', '-m'])
-#     assert parser == 'a'
-
-
-import os
-import sys
-from subprocess import getstatusoutput, getoutput
+# from subprocess import getstatusoutput, getoutput
 from src.tihttp.main import main
 
-# prg = "./hello.py"
 
 # prg = "./src/tihttp/main.py"
+
 # # --------------------------------------------------
 # def test_exists():
 #     assert os.path.isfile(prg)
 
 
-def test_myoutput(capsys):  # or use "capfd" for fd-level
-    # print("hello")
-    sys.stderr.write("world\n")
-    captured = capsys.readouterr()
-    assert captured.out == "hello\n"
-    assert captured.err == "world\n"
-    print("next")
-    captured = capsys.readouterr()
-    assert captured.out == "next\n"
+# def test_myoutput(capsys):  # or use "capfd" for fd-level
+#     # print("hello")
+#     sys.stderr.write("world\n")
+#     captured = capsys.readouterr()
+#     assert captured.out == "hello\n"
+#     assert captured.err == "world\n"
+#     print("next")
+#     captured = capsys.readouterr()
+#     assert captured.out == "next\n"
 
 
-def test_greet_cli(capsys):
-    cli(["Asia/Jakarta"])
-    captured = capsys.readouterr()
-    result = captured.out
-    assert "Hello, Jakarta!" in result
-
-
-def test_google_header(capsys):
-    main(["-H", "google.com"])
+def test_api_body(capsys):
+    main(["-B", "http://jsonplaceholder.typicode.com/todos?userId=1"])
     captured = capsys.readouterr()
     result = captured.out
-    assert "Hello, Jakarta!" in result
+    with open("tests/jsonplaceholder.json", "r") as f:
+        output = f.read()
+    assert result == output
 
 
 # --------------------------------------------------
